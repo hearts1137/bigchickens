105d33194fa62aaf6d747a68bbaf6b6ac253223e
hearts1137 <63993785+hearts1137@users.noreply.github.com>
2024-09-29 17:06:51 -0400

    Add files via upload

--- app-hooks/before-starting/20-manual_install.sh
+++ app-hooks/before-starting/20-manual_install.sh
@@ -3,7 +3,6 @@ if [ -z ${NC_MANUAL_APPS_PATH+x} ];
 then
 	NC_MANUAL_APPS_PATH=/tmp/apps
 fi
-
 # This expects unzipped/untarred folders of the appname and appname.conf with config values
 for app in `ls ${NC_MANUAL_APPS_PATH}`;
 do
@@ -12,5 +11,18 @@ do
 		echo "Manual install app - ${app}"
 		cp -r ${NC_MANUAL_APPS_PATH}/${app} apps/
 		./occ app:enable ${app}
+
+		if [ -f ${NC_MANUAL_APPS_PATH}/${app}.sh ];
+		then
+			echo "Running provided config script for app"
+			sh ${NC_MANUAL_APPS_PATH}/${app}.sh
+
+			if [ $? -ne 0 ];
+			then
+				echo "[Error] Manual install user provided script ${NC_MANUAL_APPS_PATH}/${app}.sh failed"
+			fi
+		else
+			echo "No config script provided for ${app}"
+		fi
 	fi
 done
